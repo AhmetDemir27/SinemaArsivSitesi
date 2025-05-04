2e6b5407173b864630a314903a6cf9f5358f2d78
Hatice Tekeli <tkl.hatice01@gmail.com>
2025-05-04 22:52:08 +0300

    updated: Architecture

--- ApplicationDbContext.cs
+++
@@ -1,4 +0,0 @@
-public DbSet<Movie> Movies { get; set; }
-public DbSet<Category> Categories { get; set; }
-public DbSet<Comment> Comments { get; set; }
-public DbSet<UserFavoriteMovie> FavoriteMovies { get; set; }
--- Category.cs
+++
@@ -1,7 +0,0 @@
-public class Category
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-
-    public ICollection<Movie> Movies { get; set; }
-}
